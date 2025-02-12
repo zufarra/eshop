b2c63fcfa2404b093b7c33b2040bb69aa22d8b9f
zufarra <zufarromli@gmail.com>
2025-02-12 16:05:48 +0700

    Rename from ListProduct.html to ProductList.html

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -33,6 +33,6 @@ public class ProductController {
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 }
