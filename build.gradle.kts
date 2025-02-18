b652bdc203f66c970ba8f1bc21421738ff878685
zufarra <zufarromli@gmail.com>
2025-02-18 16:01:21 +0700

    Edit the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -63,4 +64,14 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+tasks.test{
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport{
+    dependsOn(tasks.test)
 }
