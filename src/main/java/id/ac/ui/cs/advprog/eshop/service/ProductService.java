a7e8522c49b61574b7e925f04a74eaeba9c4cb14
zufarra <zufarromli@gmail.com>
2025-02-19 16:05:51 +0700

    change modifier on create method on ProductService.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,7 +4,7 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
+    Product create(Product product);
     Product edit(Product product, String productId);
     void delete(String productId);
     public Product findProductById(String productId);
