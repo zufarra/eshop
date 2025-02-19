339a991b013b3a6243d18c8b28a7bcf4194c19c3
zufarra <zufarromli@gmail.com>
2025-02-19 15:59:32 +0700

    change modifier on delete method on ProductService.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public Product edit(Product product, String productId);
-    public void delete(String productId);
+    void delete(String productId);
     public Product findProductById(String productId);
     public List<Product> findAll();
 }
