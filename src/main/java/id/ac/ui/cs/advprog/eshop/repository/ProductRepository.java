5b0ca02d55157b164905171b85a8afba5697a62f
zufarra <zufarromli@gmail.com>
2025-02-28 09:32:55 +0700

    change code on CarRepository and ProductRepository so that they just extends from the BaseRepository that has implements all method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,47 +3,6 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
-
 @Repository
-public class ProductRepository {
-    private List<Product> productData = new ArrayList<>();
-
-    public Product create(Product product) {
-        product.setProductId(UUID.randomUUID().toString());
-        productData.add(product);
-        return product;
-    }
-
-    public Product edit(Product product, String productId){
-        Product editedProduct = findProductById(productId);
-        if (editedProduct != null) {
-            editedProduct.setProductName(product.getProductName());
-            editedProduct.setProductQuantity(product.getProductQuantity());
-            return editedProduct;
-        }
-        return null;
-    }
-
-    public void delete(String productId) {
-        Product deletedProduct = findProductById(productId);
-        if (deletedProduct != null) {
-            productData.remove(deletedProduct);
-        }
-    }
-
-    public Product findProductById(String productId) {
-        for (Product product : productData) {
-            if (product.getProductId().equals(productId)) {
-                return product;
-            }
-        }
-        return null;
-    }
-    public Iterator<Product> findAll() {
-        return productData.iterator();
-    }
+public class ProductRepository extends BaseRepository<Product> {
 }
