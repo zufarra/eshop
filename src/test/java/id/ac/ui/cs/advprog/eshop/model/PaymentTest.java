bda482e511e203e57a26597ddbd4753c783f4800
zufarra <zufarromli@gmail.com>
2025-03-06 12:09:30 +0700

    [RED] Modify tests for Payment model.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -8,35 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentTest {
     @Test
-    void testCreatePaymentWithoutStatus(){
-        Map<String, String> paymentData = new HashMap<>();
-        Payment payment = new Payment("123", "VOUCHER", null, paymentData);
-        assertEquals("123", payment.getId());
-        assertEquals("VOUCHER", payment.getMethod());
-        assertNull(payment.getStatus());
-        assertTrue(payment.getPaymentData().isEmpty());
-    }
-
-    @Test
     void testCreatePaymentWithSuccessStatus(){
         Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP12345678ABC");
         Payment payment = new Payment("456", "VOUCHER", "SUCCESS", paymentData);
-
-        assertEquals("SUCCESS", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
     }
 
     @Test
     void testCreatePaymentWithInvalidStatus(){
         Map<String, String> paymentData = new HashMap<>();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             new Payment("789", "VOUCHER", "INVALID_STATUS", paymentData);
         });
-        assertEquals("Invalid payment status", exception.getMessage());
     }
 
     @Test
     void testCreatePaymentWithVoucherMethod(){
         Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("VOUCHER", "ESHOP12345678ABC");
         Payment payment = new Payment("101", "VOUCHER", "FAILED", paymentData);
         assertEquals("VOUCHER", payment.getMethod());
     }
@@ -44,6 +34,8 @@ public class PaymentTest {
     @Test
     void testCreatePaymentWithBankTransferMethod(){
         Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("bankName", "Bank ABC");
+        paymentData.put("referenceCode", "REF1234");
         Payment payment = new Payment("102", "BANK_TRANSFER", "REJECTED", paymentData);
 
         assertEquals("BANK_TRANSFER", payment.getMethod());
@@ -60,12 +52,12 @@ public class PaymentTest {
     @Test
     void testCreatePaymentWithCorrectBankTransferValue(){
         Map<String, String> paymentData = new HashMap<>();
-        paymentData.put("bankTransfer", "Bank ABC");
+        paymentData.put("bankName", "Bank ABC");
         paymentData.put("referenceCode", "REF1234");
 
         Payment payment = new Payment("104", "BANK_TRANSFER", "REJECTED", paymentData);
         assertEquals("Bank ABC", payment.getPaymentData().get("bankName"));
-        assertEquals("REF12345", payment.getPaymentData().get("referenceCode"));
+        assertEquals("REF1234", payment.getPaymentData().get("referenceCode"));
 
     }
 
@@ -93,24 +85,21 @@ public class PaymentTest {
     void testCreatePaymentWithInvalidCharacterLongForVoucherValue(){
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234AB5678");
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Payment("103", "VOUCHER", "REJECTED", paymentData);
-        });
+        Payment payment = new Payment("103", "VOUCHER", "REJECTED", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
     }
     @Test
     void testCreatePaymentWithInvalidPrefixLongForVoucherValue(){
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "1234ABC567");
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Payment("103", "VOUCHER", "REJECTED", paymentData);
-        });
+        Payment payment = new Payment("103", "VOUCHER", "REJECTED", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
     }
     @Test
     void testCreatePaymentWithInvalidNumericLongForVoucherValue(){
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC567A");
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Payment("103", "VOUCHER", "REJECTED", paymentData);
-        });
+        Payment payment = new Payment("103", "VOUCHER", "REJECTED", paymentData);
+        assertEquals("REJECTED", payment.getStatus());
     }
 }
