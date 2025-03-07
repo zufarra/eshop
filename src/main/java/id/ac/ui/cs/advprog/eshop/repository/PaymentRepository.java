36ccd08a43c82b878bb214581ed544f228dd7e37
zufarra <zufarromli@gmail.com>
2025-03-07 07:59:38 +0700

    [GREEN] Implement PaymentRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,17 +11,40 @@ public class PaymentRepository {
     private List<Payment> allPayment = new ArrayList<>();
 
     public Payment save (Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : allPayment) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                allPayment.remove(i);
+                allPayment.add(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        allPayment.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
+        for(Payment savedPayment : allPayment) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
     public List<Payment> findAll() {
-        return null;
+        List<Payment> payments = new ArrayList<>();
+        payments.addAll(allPayment);
+        return payments;
     }
 
     public List<Payment> findAllByMethod(String method) {
-        return null;
+        List<Payment> payments = new ArrayList<>();
+        for (Payment savedPayment : allPayment) {
+            if (savedPayment.getMethod().equals(method)) {
+                payments.add(savedPayment);
+            }
+        }
+        return payments;
     }
 }
