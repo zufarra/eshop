d492941d830b44e57dd566bad3b1a119246d8306
zufarra <zufarromli@gmail.com>
2025-03-07 10:47:22 +0700

    [GREEN] Implement Payment model with a new code.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -18,5 +18,22 @@ public class Payment {
     private Order order;
 
     public Payment(String method, Map<String, String> paymentData, Order order) {
+        if (!isValidMethod(method)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        this.id = UUID.randomUUID().toString();
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+    }
+
+    private boolean isValidMethod(String method) {
+        return Arrays.stream(PaymentMethod.values())
+                .map(PaymentMethod::getValue)
+                .anyMatch(validMethod -> validMethod.equals(method));
     }
 }
